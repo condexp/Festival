652ef792626f300c63a3f99fc9b7a71a7c663f64
aconde <colenthin@gmail.com>
2021-06-21 12:32:53 +0200

    creat BD ,folder , shared and techno Homcontroller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_festival")
+     */
+    public function index(): Response
+    {
+
+        
+        return $this->render('techno/home.html.twig',
+         [
+            'controller_name' => 'test festival ',
+            
+         ]   
+    
+    );
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
